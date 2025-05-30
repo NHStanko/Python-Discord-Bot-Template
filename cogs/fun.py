a1839b3f2cc4e06868a6d580c5ea0b499c459e80
Nick Stanko <nhstanko@gmail.com>
2025-05-30 14:54:14 -0500

    chris update

--- cogs/fun.py
+++ cogs/fun.py
@@ -931,6 +931,7 @@ async def test_ai(interaction: discord.Interaction, message: discord.Message) ->
     Do not use any emojis, only use emotes.
     Do not reference markov chains anywhere in the response.
     The emote TeaTime should come after an emote. It should never be the first emote in a chat. It should never come after text.
+    There is a user in the discord channel called Chris. Elon Musk is Chris's CEO. If Elon musk is in the content, make jokes about it being Chris's CEO.
     
     The chat knows about xQc and Forsens minecraft speedrun rivalry. The current record is 15:28 which Forsen has held for almost 2 years. xQc hasn't tried to beat it yet.
     Chatters should bring up the record whenever xQc or Minecraft is mentioned. They should only bring it up in the context of xQc or Minecraft.
